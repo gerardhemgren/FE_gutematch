c4d37eb2157d9e9da3a434435eaed8d15d8270ed
gerard hemgren <gerardhemgren@gmail.com>
2022-01-04 11:37:42 -0300

    fixed bug at conditional match message

--- src/services/matches.js
+++ src/services/matches.js
@@ -3,43 +3,31 @@ import axios from 'axios';
 const baseUrl = 'https://gutematch.herokuapp.com';
 // const baseUrl = 'http://localhost:5000';
 
-const getOpenMatches = (userId) => {
-    const request = axios.get(`${baseUrl}/api/open_matches/${userId}`)
-    return request
-        .then(response => response.data)
+async function getOpenMatches(userId) {
+    return await axios.get(`${baseUrl}/api/open_matches/${userId}`).then(response => response.data)
 }
 
-const getMyMatches = (userId) => {
-    const request = axios.get(`${baseUrl}/api/my_matches/${userId}`)
-    return request
-        .then(response => response.data)
+async function getMyMatches(userId) {
+    return await axios.get(`${baseUrl}/api/my_matches/${userId}`).then(response => response.data)  
 }
 
-const createMatch = (matchInfo, userId) => {
-    const request = axios.post(`${baseUrl}/api/create_match/${userId}`, matchInfo)
-    return request
-        .then(response => response.data)
+async function createMatch(matchInfo, userId){
+    return await axios.post(`${baseUrl}/api/create_match/${userId}`, matchInfo) .then(response => response.data)       
 }
 
-const deleteMatch = (matchId, userId) => {
+async function deleteMatch(matchId, userId) {
     const match = { id_match: matchId }
-    const request = axios.delete(`${baseUrl}/api/my_matches/owner/${userId}`, { data: match })
-    return request
-        .then(response => response.data)
+    return await axios.delete(`${baseUrl}/api/my_matches/owner/${userId}`, { data: match }).then(response => response.data)
 }
 
-const joinMatch = (matchId, userId) => {
+async function joinMatch(matchId, userId) {
     const match = { id_match: matchId }
-    const request = axios.post(`${baseUrl}/api/open_matches/${userId}`, match)
-    return request
-        .then(response => response.data)
+    return await axios.post(`${baseUrl}/api/open_matches/${userId}`, match).then(response => response.data)        
 }
 
-const leaveMatch = (matchId, userId) => {
+async function leaveMatch(matchId, userId) {
     const match = { id_match: matchId }
-    const request = axios.delete(`${baseUrl}/api/my_matches/${userId}`, { data: match })
-    return request
-        .then(response => response.data)
+    return await axios.delete(`${baseUrl}/api/my_matches/${userId}`, { data: match }).then(response => response.data)   
 }
 
 const exportObj = { getOpenMatches, getMyMatches, createMatch, deleteMatch, joinMatch, leaveMatch }
